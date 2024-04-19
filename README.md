92710c71e486c9d997dc04543090ab0810c89ffc
DarthLilo <darthdaxx@comcast.net>
2024-04-18 19:23:05 -0700

    add beta downloading support

---
+++ README.md
@@ -0,0 +1,6 @@
+# PBLC Update Manager
+
+This is a personal project I started to help manage Lethal Company mods with friends, this is not *designed* for public use but I won't stop you from using it.
+
+## IMPORTANT
+Currently it is in an extremely unstable and usuable state, the version uploaded to this github is a WIP version which won't work atm, the only full working build is accessible through Discord.
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from urllib import request
 pyglet.font.add_file('Minecraft_Five.otf')
 pyglet.font.add_file('Minecraft_Five_Bold.otf')
 
-github_repo_versoin_db = "https://raw.githubusercontent.com/DarthLilo/PBLC-Update-Manager/main/version_db.json"
+github_repo_versoin_db = "https://raw.githubusercontent.com/DarthLilo/PBLC-Update-Manager/master/version_db.json"
 
 def read_reg(ep, p = r"", k = ''):
     try:
@@ -57,7 +57,6 @@ def newEmptyRow(self,row_number,spacing):
         self.empty_row = customtkinter.CTkLabel(self.main_frame, text="")
         self.empty_row.grid(row=row_number, column=0, padx=20, pady=spacing)
 
-github_repo_json = json.loads(request.urlopen(github_repo_versoin_db).read().decode())
 steam_install_path = str(read_reg(ep = winreg.HKEY_LOCAL_MACHINE, p = r"SOFTWARE\\Wow6432Node\\Valve\\Steam", k = 'InstallPath'))
 steamapps = steam_install_path+"\steamapps"
 library_folders = steamapps+"\libraryfolders.vdf"
@@ -79,7 +78,6 @@ def download_from_google_drive(source,destination):
 
 LC_Path = locate_lethal_company()
 downloads_folder = os.path.normpath(f"{LC_Path}/downloads")
-latest_update = github_repo_json
 pblc_vers = os.path.normpath(f"{LC_Path}/pblc_version")
 bepinex_path = os.path.normpath(f"{LC_Path}/BepInEx")
 doorstop_path = os.path.normpath(f"{LC_Path}/doorstop_config.ini")
@@ -89,14 +87,16 @@ winhttp_path = os.path.normpath(f"{LC_Path}/winhttp.dll")
 
 if os.path.exists(pblc_vers):
     installed_version = str(open_json(pblc_vers)['version']).replace(".","")
+    installed_beta_version = str(open_json(pblc_vers)['beta_version']).replace(".","")
 else:
     installed_version = "0"
+    installed_beta_version = "0"
 
-latest_version = str(latest_update['version']).replace(".","")
 
-def download_update():
-    latest_update_zip = f"{downloads_folder}/{latest_update['name']}.zip"
-    download_from_google_drive(latest_update['source'],latest_update_zip)
+
+def download_update(update_data):
+    latest_update_zip = f"{downloads_folder}/{update_data['name']}.zip"
+    download_from_google_drive(update_data['source'],latest_update_zip)
     return latest_update_zip
 
 def decompress_zip(latest_update_zip):
@@ -107,14 +107,14 @@ def decompress_zip(latest_update_zip):
     shutil.rmtree(downloads_folder)
     print("Updating version cache...")
 
-def startUpdate():
+def startUpdate(update_data,update_type):
     try:
         print("Update Available, beginning download...")
 
         if not os.path.exists(downloads_folder):
             os.mkdir(downloads_folder)
 
-        latest_update_zip = download_update()
+        latest_update_zip = download_update(update_data)
 
         clear_files = [bepinex_path,doorstop_path,winhttp_path]
         for i in range(0,3):
@@ -127,26 +127,65 @@ def startUpdate():
         
         decompress_zip(latest_update_zip)
 
-        #with open(pblc_vers, "w") as pblc_vers_upd:
-        #    pblc_vers_upd.write(json.dumps({"version":latest_update['version']}))
+        current_installed_versions = open_json(pblc_vers)
+
+        if update_type == "release":
+            current_installed_versions["version"] = update_data['version']
+            current_installed_versions["beta_version"] = "0.0.0"
+        else:
+            current_installed_versions["beta_version"] = update_data['beta_version']
+            current_installed_versions["version"] = "0.0.0"
+            
+        with open(pblc_vers, "w") as pblc_vers_upd:
+            pblc_vers_upd.write(json.dumps(current_installed_versions))
 
         ctypes.windll.user32.MessageBoxW(0, "Succsessfully installed update!", "PBLC Update Manager")
     except:
         ctypes.windll.user32.MessageBoxW(0, "Error (ask lilo this message isn't supposed to show up LMFAO)", "PBLC Update Manager")
 
-def checkForUpdates():
-    if installed_version < latest_version:
-        print("Update Found.")
-        prompt_answer = ctypes.windll.user32.MessageBoxW(0,f"An update is available, would you like to install it?\n\n\n{latest_update['description']}\nReleased: {latest_update['release_date']}\nVersion: {latest_update['version']}","PBLC Update Manager",4)
-        if prompt_answer == 6:
-            startUpdate()
-    elif not os.path.exists(bepinex_path) or not os.path.exists(doorstop_path) or not os.path.exists(winhttp_path):
-        print("Vanilla or broken version found.")
-        prompt_answer = ctypes.windll.user32.MessageBoxW(0,f"Vanilla or broken version detected, would you like to install the latest mods?\n\n\n{latest_update['description']}\nReleased: {latest_update['release_date']}\nVersion: {latest_update['version']}","PBLC Update Manager",4)
-        if prompt_answer == 6:
-            startUpdate()
+def checkForUpdates(update_type):
+
+    print("Checking for updates...")
+
+    #fetching latest version
+    #github_repo_json = json.loads(request.urlopen(github_repo_versoin_db).read().decode())
+    github_repo_json = open_json("version_db.json")
+    latest_version = str(github_repo_json[update_type]['version']).replace(".","") if update_type == "release" else str(github_repo_json[update_type]['beta_version']).replace(".","")
+
+
+    #RELEASE
+    if update_type == "release":
+
+        if installed_version < latest_version:
+            print("New Update Found.")
+            prompt_answer = ctypes.windll.user32.MessageBoxW(0,f"An update is available, would you like to install it?\n\n\n{github_repo_json[update_type]['description']}\nReleased: {github_repo_json[update_type]['release_date']}\nVersion: {github_repo_json[update_type]['version']}","PBLC Update Manager",4)
+            if prompt_answer == 6:
+                startUpdate(github_repo_json[update_type],update_type)
+        elif not os.path.exists(bepinex_path) or not os.path.exists(doorstop_path) or not os.path.exists(winhttp_path):
+            print("Vanilla or broken version found.")
+            prompt_answer = ctypes.windll.user32.MessageBoxW(0,f"Vanilla or broken version detected, would you like to install the latest mods?\n\n\n{github_repo_json[update_type]['description']}\nReleased: {github_repo_json[update_type]['release_date']}\nVersion: {github_repo_json[update_type]['version']}","PBLC Update Manager",4)
+            if prompt_answer == 6:
+                startUpdate(github_repo_json[update_type],update_type)
+        else:
+            print("No updates found.")
+            ctypes.windll.user32.MessageBoxW(0, "No updates available.", "PBLC Update Manager")
+    
+
+    #BETA
     else:
-        ctypes.windll.user32.MessageBoxW(0, "No updates available.", "PBLC Update Manager")
+        if installed_beta_version < latest_version:
+            print("New Beta Found.")
+            prompt_answer = ctypes.windll.user32.MessageBoxW(0,f"A new beta is available, would you like to install it?\n\n\n{github_repo_json[update_type]['description']}\nReleased: {github_repo_json[update_type]['release_date']}\nBeta Version: {github_repo_json[update_type]['beta_version']}\nVersion: {github_repo_json[update_type]['version']}","PBLC Update Manager",4)
+            if prompt_answer == 6:
+                startUpdate(github_repo_json[update_type],update_type)
+        elif not os.path.exists(bepinex_path) or not os.path.exists(doorstop_path) or not os.path.exists(winhttp_path):
+            print("Vanilla or broken version found.")
+            prompt_answer = ctypes.windll.user32.MessageBoxW(0,f"Vanilla or broken version detected, would you like to install the latest beta mods?\n\n\n{github_repo_json[update_type]['description']}\nReleased: {github_repo_json[update_type]['release_date']}\nBeta Version: {github_repo_json[update_type]['beta_version']}\nVersion: {github_repo_json[update_type]['version']}","PBLC Update Manager",4)
+            if prompt_answer == 6:
+                startUpdate(github_repo_json[update_type],update_type)
+        else:
+            print("No updates found.")
+            ctypes.windll.user32.MessageBoxW(0, "No updates available.", "PBLC Update Manager")
 
 #UI MANAGEMENT
 class PBLCApp(customtkinter.CTk):
@@ -160,8 +199,6 @@ class PBLCApp(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        has_checked_for_updates = False
-
         #images
         self.ice_cube_logo = customtkinter.CTkImage(Image.open(resource_path("pill_bottle.jpg")),size=(1050, 100))
 
@@ -173,7 +210,7 @@ class PBLCApp(customtkinter.CTk):
         self.main_frame_logo = customtkinter.CTkLabel(self.main_frame, text="", image=self.ice_cube_logo)
         self.main_frame_logo.grid(row=0, column=0, padx=20, pady=20)
 
-        newEmptyRow(self,1,10)
+        newEmptyRow(self,1,5)
 
         self.lethal_install_border = customtkinter.CTkFrame(self.main_frame,width=100,height=100,fg_color="#191919")
         self.lethal_install_border.grid_columnconfigure(0, weight=1)
@@ -185,24 +222,24 @@ class PBLCApp(customtkinter.CTk):
         self.lethal_install_path = customtkinter.CTkLabel(self.lethal_install_border,text=LC_Path)
         self.lethal_install_path.grid(row=1, column=0,padx=15,pady=10)
 
-        newEmptyRow(self,1,10)
+        newEmptyRow(self,1,40)
 
         self.actions_border = customtkinter.CTkFrame(self.main_frame,width=100,height=100,fg_color="#191919")
         self.actions_border.grid_columnconfigure(0, weight=1)
         self.actions_border.grid(row=2, column=0)
 
-        if has_checked_for_updates == False:
+        self.update_button_main = customtkinter.CTkButton(self.actions_border, text="Check For Updates",command=self.check_for_updates_main)
+        self.update_button_main.grid(row=0, column=0, padx=20, pady=20)
 
-            self.update_button_main = customtkinter.CTkButton(self.actions_border, text="Check For Updates",command=self.button_click)
-            self.update_button_main.grid(row=0, column=0, padx=20, pady=20)
+        self.update_button_main_2 = customtkinter.CTkButton(self.actions_border, text="Check For Updates (BETA)",command=self.check_for_updates_beta)
+        self.update_button_main_2.grid(row=0, column=1, padx=20, pady=20)
 
-            self.update_button_main = customtkinter.CTkButton(self.actions_border, text="Check For Updates (BETA)",command=self.button_click)
-            self.update_button_main.grid(row=0, column=1, padx=20, pady=20)
-
-    # add methods to app
-    def button_click(self):
-        has_checked_for_updates = True
-        print(has_checked_for_updates)
+    # click_methods
+    def check_for_updates_main(self):
+        checkForUpdates("release")
+    
+    def check_for_updates_beta(self):
+        checkForUpdates("beta")
 
 app = PBLCApp()
 app.mainloop()
