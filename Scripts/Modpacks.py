95a2bbc1e7006fa5e285227780ade72b39ae1aa3
DarthLilo <darthdaxx@comcast.net>
2024-10-09 00:30:07 -0700

    mod updater and caching

--- Scripts/Modpacks.py
+++ Scripts/Modpacks.py
@@ -34,7 +34,6 @@ class Modpacks:
             "name": name,
             "version": "1.0.0",
             "update_date": Time.CurrentDate(),
-            "packages": []
         }
 
         with open(f"{modpack_location}/modpack.json",'w') as modpack_json:
@@ -43,6 +42,9 @@ class Modpacks:
 
         return
     
+    def GetJson(author,name):
+        return Util.OpenJson(f"{Modpacks.Path(author,name)}/modpack.json")
+
     def Select(author,name):
         if not os.path.exists(Modpacks.Path(author,name)):
             Logging.New(f"Modpack {author}-{name} not found!",'error')
@@ -66,24 +68,53 @@ class Modpacks:
         plugins_folder = f"{Cache.SelectedModpack}/BepInEx/plugins"
         for file in os.listdir(plugins_folder):
             if os.path.isdir(f"{plugins_folder}/{file}") and os.path.exists(f"{plugins_folder}/{file}/mod.json"):
-                Modpacks.LoadMod(f"{plugins_folder}/{file}")
+                Modpacks.Mods.LoadMod(f"{plugins_folder}/{file}")
     
-    def LoadMod(path):
-        full_mod_name = os.path.basename(path).split("-")
-        author = full_mod_name[0]
-        name = full_mod_name[1]
+    def Export(author,name):
+        export_json_loc = f"{Modpacks.Path(author,name)}/{author}-{name}.json"
+        modpack_json = Modpacks.GetJson(author,name)
 
-        new_mod = {
-            "json_file": f"{path}/mod.json",
-            "icon": f"{path}/icon.png",
-            }
+        export_data = {
+            "author": author,
+            "name": name,
+            "version": modpack_json['version'],
+            "update_date": modpack_json['update_date'],
+            "contents": {
+                "thunderstore_packages": [],
+                "overrides": []
+            },
+            "reinstall": False
+        }
 
-        Cache.LoadedMods[f"{author}-{name}"] = new_mod
-    
+        for mod in Cache.LoadedMods:
+            mod_json = Util.OpenJson(Cache.LoadedMods[mod]['json_file'])
+            export_data['contents']['thunderstore_packages'].append({"author": mod_json['author'],"name": mod_json['name'],"mod_version": mod_json['mod_version']})
+
+
+
+        Util.WriteJson(export_json_loc,export_data)
+
+        return
+
     class Mods:
         def Path(author,name,mod_version):
             return f"{Cache.SelectedModpack}/BepInEx/plugins/{author}-{name}-{mod_version}"
         
+        def LoadMod(path):
+            full_mod_name = os.path.basename(path).split("-")
+            author = full_mod_name[0]
+            name = full_mod_name[1]
+
+            new_mod = {
+                "json_file": f"{path}/mod.json",
+                "icon": f"{path}/icon.png",
+                }
+
+            Cache.LoadedMods[f"{author}-{name}"] = new_mod
+
+        def UnloadMod(author,name):
+            del Cache.LoadedMods[f"{author}-{name}"]
+
         def AddPackageFiles(author,name,mod_version,files):
             target_mod_json = f"{Modpacks.Mods.Path(author,name,mod_version)}/mod.json"
             mod_json = {}
@@ -103,11 +134,15 @@ class Modpacks:
 
             mod_location, author, name, mod_version, mod_files = Thunderstore.Download(url)
             Modpacks.Mods.AddPackageFiles(author,name,mod_version,mod_files)
+            Modpacks.Mods.LoadMod(mod_location)
         
-        def Delete(author,name,mod_version):
+        def Delete(author,name,mod_version=""):
             if not os.path.exists(Cache.SelectedModpack):
                 Logging.New("Please select a valid modpack first!",'error')
                 return
+            
+            if not mod_version.strip():
+                mod_version = Modpacks.Mods.GetVersion(author,name)
 
             mod_path = Modpacks.Mods.Path(author,name,mod_version)
 
@@ -131,6 +166,8 @@ class Modpacks:
                     os.remove(f"{mod_path}/{file}")
 
             shutil.rmtree(mod_path)
+            
+            Modpacks.Mods.UnloadMod(author,name)
         
         def Json(author,name):
             if f"{author}-{name}" in Cache.LoadedMods:
@@ -138,13 +175,33 @@ class Modpacks:
             
             return {}
 
+        def Installed(author,name):
+            return f"{author}-{name}" in Cache.LoadedMods
+
         def CheckForUpdates(author, name):
 
+            if not Modpacks.Mods.Installed(author, name):
+                Logging.New(f"Invalid mod, [{author}-{name}] isn't installed!")
+                return False
+            
             cur_mod_data = Modpacks.Mods.Json(author,name)
 
             latest_data = Cache.Get(author,name)
-            print(cur_mod_data)
-            if Networking.CompareVersions(latest_data['version_number'],cur_mod_data['mod_version']):
-                Logging.New(f"{author}-{name} has updates!")
-            else:
-                Logging.New(f"{author}-{name} found no new updates")
+
+            return Networking.CompareVersions(latest_data['version_number'],cur_mod_data['mod_version'])
+        
+        def GetVersion(author, name):
+            return Modpacks.Mods.Json(author,name)["mod_version"]
+
+        def Update(author,name,mod_version=""):
+
+            if not Modpacks.Mods.Installed(author, name):
+                Logging.New(f"Invalid mod, [{author}-{name}] isn't installed!")
+                return False
+            
+            Logging.New(f"Updating [{author}-{name}]...")
+            
+            Modpacks.Mods.Delete(author,name,Modpacks.Mods.GetVersion(author,name))
+            Modpacks.Mods.Add(f"https://thunderstore.io/c/lethal-company/p/{author}/{name}/{mod_version}")
+
+            Logging.New(f"Finished updating [{author}-{name}]")
