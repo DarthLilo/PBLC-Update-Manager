99736f9ffb65ca6e5abd7306f8259b77c6626282
DarthLilo <darthdaxx@comcast.net>
2024-10-17 19:24:33 -0700

    thread frame element

---
+++ Scripts/UIElements/ThreadFrame.py
@@ -0,0 +1,51 @@
+from PyQt6.QtCore import (
+    Qt, QSize)
+
+from PyQt6.QtWidgets import QGridLayout, QLabel, QFrame, QSizePolicy, QHBoxLayout, QSpacerItem, QWidget, QPushButton, QVBoxLayout, QProgressBar
+from PyQt6.QtGui import QColor, QPalette, QPixmap, QFont, QIcon
+
+import os, random
+
+class ThreadFrame(QFrame):
+    def __init__(self, parent=None, thread_index=0):
+        super().__init__()
+
+        self._layout = QHBoxLayout(self)
+        self._thread_index = thread_index
+
+        self.setFrameShape(QFrame.Shape.StyledPanel)
+        self.setSizePolicy(QSizePolicy.Policy.Expanding,QSizePolicy.Policy.Fixed)
+
+        self.draw()
+
+    def draw(self):
+
+        self.thread_icon_label = QLabel("thread_icon_label")
+        self.thread_icon_label.setPixmap(QPixmap("E:\\Lilos Coding\\PBML\\assets\\inactive_thread.png").scaled(QSize(105,105),aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio,transformMode=Qt.TransformationMode.SmoothTransformation))
+        self._layout.addWidget(self.thread_icon_label)
+
+        self.thread_info_container = QFrame()
+        self.thread_info_layout = QVBoxLayout(self.thread_info_container)
+        self.thread_info_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+
+        self.thread_name_label = QLabel(f"Inactive Thread {self._thread_index}")
+        self.thread_name_label.setFont(QFont("IBM 3270", 12))
+        self.thread_info_layout.addWidget(self.thread_name_label)
+
+        self.thread_progress_bar = QProgressBar()
+        self.thread_progress_bar.setValue(0)
+        self.thread_progress_bar.setFont(QFont("IBM 3270", 10))
+        self.thread_info_layout.addWidget(self.thread_progress_bar)
+        
+        self._layout.addWidget(self.thread_info_container)
+    
+    def setIcon(self,filepath):
+        self.thread_icon_label.setPixmap(QPixmap(filepath).scaled(QSize(105,105),aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio,transformMode=Qt.TransformationMode.SmoothTransformation))
+    
+    def setName(self, name):
+        self.thread_name_label.setText(str(name))
+    
+    def setProgress(self,progress):
+        """Whole value ints for progress, 50% would be 50"""
+        self.thread_progress_bar.setValue(int(progress))
