8246570e93c7581768fce6b124963cc613d6932f
DarthLilo <darthdaxx@comcast.net>
2024-11-04 17:34:49 -0800

    update scroll menu

--- Scripts/UIElements/ModpackScrollMenu.py
+++ Scripts/UIElements/ModpackScrollMenu.py
@@ -25,8 +25,8 @@ class ModpackScrollMenu(QScrollArea):
 
         return
     
-    def addModpack(self, modpack_icon = "",modpack_name = "Modpack", modpack_author="Author",modpack_version="1.0.0",mod_count=0):
-        modpack_frame = ModpackFrame(modpack_icon=modpack_icon,modpack_name=modpack_name,modpack_author=modpack_author,modpack_version=modpack_version,mod_count=mod_count,xyCords=(self._xPos,self._yPos))
+    def addModpack(self, modpack_icon = "",modpack_name = "Modpack", modpack_author="Author",modpack_version="1.0.0",mod_count=0,screen_func=None):
+        modpack_frame = ModpackFrame(modpack_icon=modpack_icon,modpack_name=modpack_name,modpack_author=modpack_author,modpack_version=modpack_version,mod_count=mod_count,xyCords=(self._xPos,self._yPos),screen_func=screen_func)
         modpack_frame.setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Fixed)
         self._grid_layout.addWidget(modpack_frame,self._xPos,self._yPos)
         self.modpacks.append(modpack_frame)
@@ -34,6 +34,13 @@ class ModpackScrollMenu(QScrollArea):
         if self._yPos >= 5:
             self._yPos = 0
             self._xPos += 1
+    
+    def clearModpacks(self):
+        self.modpacks.clear()
+        for i in reversed(range(self._grid_layout.count())):
+            self._grid_layout.itemAt(i).widget().setParent(None)
+        self._xPos = 0
+        self._yPos = 0
     
     def getModpacks(self):
         return self.modpacks
