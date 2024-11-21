0f0f864931c70405ae9ab246423c1ef23d60fa0b
DarthLilo <darthdaxx@comcast.net>
2024-11-20 22:42:18 -0800

    version update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ else:
 
 
 ReleaseTag = "Stable"
-PBLCVersion = "1.0.6"
+PBLCVersion = "1.1.0"
 ProgramDataFolder = os.path.normpath(f"{CurFolder}/ProgramData")
 LoggingFolder = os.path.normpath(f"{ProgramDataFolder}/Logs")
 ModpacksFolder = os.path.normpath(f"{ProgramDataFolder}/Modpacks")
